82af379ab86f9c53cf8f3c8e111a58d436362016
MarcP70 <marcpourias@yahoo.fr>
2023-08-21 12:58:07 +0200

    Fix: file 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,34 +2,42 @@
 """
 Gathers data from an API based on employee ID
 """
-
 import requests
 from sys import argv
 
 
 def completed_todos_from_employee_id(employee_id):
+    """
+    Retrieves and displays the completed tasks of a specific employee
+    from an API.
 
-    base_url = "https://jsonplaceholder.typicode.com/"
+    Args:
+        employee_id (int): The ID of the employee whose completed tasks need
+        to be retrieved.
 
-    # Fetch employee data
-    user_response = requests.get(base_url + "users/{}".format(employee_id))
-    todos_response = requests.get(
-        base_url + "todos", params={"userId": employee_id})
+    Returns:
+        None
+    """
+    base_url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{base_url}/users/{employee_id}'
+    todos_url = f'{base_url}/todos?userId={employee_id}'
+
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
 
     user_data = user_response.json()
     todos_data = todos_response.json()
 
     EMPLOYEE_NAME = user_data['name']
-    print(type(EMPLOYEE_NAME))
     TOTAL_NUMBER_OF_TASKS = len(todos_data)
-    completed_todos = [task for task in todos_data if task.get('completed')]
-    NUMBER_OF_DONE_TASKS = len(completed_todos)
+    NUMBER_OF_DONE_TASKS = sum(1 for task in todos_data if task['completed'])
 
-    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
-          NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    for task in completed_todos:
-        TASK_TITLE = task.get('title')
-        print("\t {}".format(TASK_TITLE))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    for task in todos_data:
+        if task['completed']:
+            print(f'\t {task["title"]}')
 
 
 if __name__ == '__main__':
