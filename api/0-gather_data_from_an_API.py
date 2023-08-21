c5c2619c47009b0024905cb35bb4319d842c7bc5
MarcP70 <marcpourias@yahoo.fr>
2023-08-21 13:07:22 +0200

    Fix: file 0-gather_data_from_an_API.py that add a main function

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 """
-Gathers data from an API based on employee ID
+Write a Python script that, using this REST API, for a given employee ID\
+returns information about his/her TODO list progress.
 """
+
 import requests
 import sys
 
 
-def completed_todos_from_employee_id(employee_id):
+def fetch_employee_data(employee_id):
     """
-    Retrieves and displays the completed tasks of a specific employee
-    from an API.
+    Fetches user data and todos data for a given employee ID from an API.
 
     Args:
-        employee_id (int): The ID of the employee whose completed tasks need
-        to be retrieved.
-
-    Returns:
-        None
+        employee_id (int): The ID of the employee.
     """
     base_url = 'https://jsonplaceholder.typicode.com'
     user_url = f'{base_url}/users/{employee_id}'
@@ -27,6 +24,20 @@ def completed_todos_from_employee_id(employee_id):
 
     user_data = user_response.json()
     todos_data = todos_response.json()
+
+    return user_data, todos_data
+
+
+def main():
+    """
+    The main function is the entry point of the script.
+    It takes an employee ID as a command-line argument,
+    fetches user data and todos data for that employee from
+    an API using the fetch_employee_data function,
+    and then prints a summary of the employee's completed tasks.
+    """
+    employee_id = int(sys.argv[1])
+    user_data, todos_data = fetch_employee_data(employee_id)
 
     EMPLOYEE_NAME = user_data['name']
     TOTAL_NUMBER_OF_TASKS = len(todos_data)
@@ -39,6 +50,5 @@ def completed_todos_from_employee_id(employee_id):
         if task['completed']:
             print(f'\t {task["title"]}')
 
-
 if __name__ == '__main__':
-    completed_todos_from_employee_id(int(sys.argv[1]))
+    main()
